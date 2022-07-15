b308d2ef2b699721b5a52bfe8e9b1fefd70d3335
chenwu <1254335319@qq.com>
2022-07-15 17:17:32 +0800

    hot-fix first commit

--- src/main/java/com/chen/git/git_test.java
+++ src/main/java/com/chen/git/git_test.java
@@ -4,5 +4,6 @@ public class git_test {
     public static void main(String[] args) {
         System.out.println("hello.txt");
         System.out.println("he;lo.txt1");
+        System.out.println("he;lo.txt222");
     }
 }
