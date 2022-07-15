5e0a2886351a5d708c209fe90b804829ce571fac
chenwu <1254335319@qq.com>
2022-07-15 17:10:53 +0800

    second commit

--- src/main/java/com/chen/git/git_test.java
+++ src/main/java/com/chen/git/git_test.java
@@ -3,5 +3,6 @@ package com.chen.git;
 public class git_test {
     public static void main(String[] args) {
         System.out.println("hello.txt");
+        System.out.println("he;lo.txt1");
     }
 }
